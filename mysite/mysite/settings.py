f734266d9efb9a00850b4d0895cb4a7dd563921d
Александр Мартюшов <caspier@rambler.ru>
2022-06-16 11:12:54 +0300

    регистрация приложения

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'mysite',
+    'news.apps.NewsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
