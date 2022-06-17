792bd002f0dad46fecd9e28f754cb8b9332a1b54
Александр Мартюшов <caspier@rambler.ru>
2022-06-17 14:13:41 +0300

    Рендеринг шаблонов

--- news/views.py
+++ news/views.py
@@ -4,12 +4,10 @@ from django.http import HttpResponse
 from .models import News
 
 def index(request):
-    news = News.objects.all()
-    res = '<h1>Список новостей</h1>'
-    for item in news:
-        res += f'<div>\n<p>{item.title}<p>\n<p>{item.content}<p>\n</div>\n<hr>\n'
-    return HttpResponse(res)
+    news = News.objects.order_by('-created_ad')
+    context = {
+        'news': news,
+        'title': 'Список новостей'
+    }
+    return render(request,template_name= 'news/index.html', context= context)
 
-
-def test(request):
-    return HttpResponse('<h1>Тестовая страница</h1>')
